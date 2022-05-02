936517973fb4c17b16bc99082701d81d6e7ad552
ritaraher <ritaraher@gmail.com>
2022-05-02 15:39:13 +0100

    trying out streamlit

--- notebooks/dashboard.py
+++ notebooks/dashboard.py
@@ -1,31 +1,22 @@
-import dash
-from dash import dcc
-from dash import html
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
-import plotly.graph_objs as go
-import plotly.express as px
-import numpy as np
-import pandas as pd
-import pickle
-import csv
+import streamlit as st
 
 
+header = st.container()
+dataset = st.container()
+features = st.container()
+model_training = st.container()
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-
-#df_fertiliser = pd.read_pickle("../data/df_fertiliser.pkl")
-#df_tweets= pd.read_csv("../data/farmtweets.csv")
-
-#df = pd.read_csv('/Users/ritaraher/Dropbox/Study/CCT/python_msc/Msc_CA2/notebooks/restaurants_zomato.csv',encoding="ISO-8859-1")
-data = {'Name':['Tom', 'nick', 'krish', 'jack'],
-        'Age':[20, 21, 19, 18]}
- 
-# Create DataFrame
-df = pd.DataFrame(data)
-
-print("Hello world!")
-
+with header:
+    st.title("This is a test")
+    st.text("this some textx")
+   
+with dataset:
+    st.header("Fertiliser dataset")
+    
+with features:
+    st.header("features")
+    
+with model_training:
+    st.header("models")
+    
