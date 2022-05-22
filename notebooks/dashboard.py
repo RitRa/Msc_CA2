e310849af6c32bbdf0f669b30325c40586fdf31e
ritaraher <ritaraher@gmail.com>
2022-05-22 19:47:59 +0100

    improved sentiment analysis, and better topics, write up for algorithms, fixing the dashboard

--- notebooks/dashboard.py
+++ notebooks/dashboard.py
@@ -46,7 +46,7 @@ col1, col2 = st.columns(2)
 page2 = st.container()
 dataset = st.container()
 features = st.container()
-model_training = st.container()
+
 
 #fertiliser
 df_fertiliser = pd.read_csv("https://raw.githubusercontent.com/RitRa/Msc_CA2/master/data/df_fertiliser.csv", encoding='latin-1')
@@ -57,8 +57,8 @@ df_all = pd.read_csv("https://raw.githubusercontent.com/RitRa/Msc_CA2/master/dat
 
 # Using "with" notation
 with st.sidebar:
-    selected = option_menu("Main Menu", ['Overview', 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected = option_menu("Main Menu", ['Overview', 'Models',], 
+        icons=['', ''], menu_icon="cast", default_index=1)
     #selected
     
 if selected == 'Overview' :
@@ -101,19 +101,16 @@ if selected == 'Overview' :
         fig_bar_count = px.bar(df_fertiliser_count, x="year", y="fertiliser_type", title='Number of fertiliser types for sale' )
         
         st.plotly_chart(fig_bar_count, use_container_width=True)
+        
+        
     
     ################## End of barplot of the number of fertiliser types ##################
-
-
-##################  Page 2 ##################  
-elif selected == 'Settings' :
-    with page2:
+    
         select_type = st.selectbox(
-            'Select fertiliser',
-            (df_fertiliser['fertiliser_type']))
+                'Select fertiliser type',
+                (df_fertiliser['fertiliser_type']))
         st.write('You selected:', select_type)  
-    
-    
+            
     with col1:
 
         ################## Start of line chart and select box ##################
@@ -142,106 +139,110 @@ elif selected == 'Settings' :
         # disply line Chart
         st.plotly_chart(fig1, use_container_width=True)
         ################## End of histogram ##################
-    
+
+
+##################  Page 2 ##################  
+elif selected == 'Models' :
+    with page2:
+        
         
 
-with model_training:
 
-    sel_col, disp_col = st.columns(2)
-    
-    # independant and dependant variables
-    X_new= df_all.drop(['fertiliser_price', 'month_year', 'Unnamed: 0'], axis=1) # feature matrix 
-    feature_name = X_new.columns
-    y_new = df_all['fertiliser_price']
-    class_labels = np.unique(y_new)
-    
-
-    # columns to encode 
-    encoder = ce.OrdinalEncoder(cols=[ 
-                                    'fertiliser_type',
-                                    'month', 
-                                    'year', 'date'])
-
-    X_new = encoder.fit_transform(X_new)
-    
-    #columns to one hot encode
-    ct = ColumnTransformer(
-        [('one_hot_encoder', OneHotEncoder(categories='auto'), [0, 1, 2, 3, 4])],   # The column numbers to be transformed (here is [0] but can be [0, 1, 3])
-        remainder='passthrough'                                         # Leave the rest of the columns untouched
-    )
-
-    X_new =ct.fit_transform(X_new)
-    
-    #splitting data
-    from sklearn.model_selection import train_test_split # Import train_test_split function [2]
-    X_train, X_test, y_train, y_test = train_test_split(X_new, y_new, test_size=0.33, random_state=0) # 67% training and 33% test // found out by 1 - test_size = 1 - 0.33 = 0.67 -> 67%
+        sel_col, disp_col = st.columns(2)
         
-    #scaling
-    sc = StandardScaler(with_mean=False)
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-
-    
-
-#################### start select a model and show a plot###############################
-    
-    models = {'Regression': LinearRegression,
-            'Lasso': Lasso,
-            'Ridge': Ridge, 
-            'ElasticNet': ElasticNet} 
+        # independant and dependant variables
+        X_new= df_all.drop(['fertiliser_price', 'month_year', 'Unnamed: 0'], axis=1) # feature matrix 
+        feature_name = X_new.columns
+        y_new = df_all['fertiliser_price']
+        class_labels = np.unique(y_new)
         
-    st.subheader("Predicted the price of feriliser")
-    select_model = st.selectbox(
-                'Select model:',
-                ("Regression", "Lasso", "Ridge", "ElasticNet"))
-        
-    st.write('You selected:', select_model)  
-    
 
-    # function that takes in the select model, list of models, test and training data
-    def plotmodel(select_model, models, X_train, y_train, X_test, y_test):
-        for key, value in models.items():
-                #st.write(i)
-            if select_model == key:
-                    
-                # model
-                model = value()
-                model.fit(X_train, y_train)
-                #Predict the response for test dataset
-                y_pred = model.predict(X_test)
-                
-                # Dataframe of results
-                model_result=pd.DataFrame({ 'Actual':y_test, 'Predicted':y_pred, 'Difference': (y_test - y_pred)})
-                st.write(model_result.head(5))
-                
-                mae = mean_absolute_error(y_test,y_pred)
-                st.write("mean_absolute_error", mae)
-                mse = mean_squared_error(y_test,y_pred)
-                st.write("mean_squared_error", mse)
-                model_rmse = math.sqrt(mse)
-                st.write("Root mean squared error", model_rmse)
-                r2 = r2_score(y_test,y_pred)
-                st.write("R2 score:", r2)
-                precision = model.score(X_train, y_train)
-                st.write("Training model precision:", precision)
-                
+        # columns to encode 
+        encoder = ce.OrdinalEncoder(cols=[ 
+                                        'fertiliser_type',
+                                        'month', 
+                                        'year', 'date'])
+
+        X_new = encoder.fit_transform(X_new)
+        
+        #columns to one hot encode
+        ct = ColumnTransformer(
+            [('one_hot_encoder', OneHotEncoder(categories='auto'), [0, 1, 2, 3, 4])],   # The column numbers to be transformed (here is [0] but can be [0, 1, 3])
+            remainder='passthrough'                                         # Leave the rest of the columns untouched
+        )
+
+        X_new =ct.fit_transform(X_new)
+        
+        #splitting data
+        from sklearn.model_selection import train_test_split # Import train_test_split function [2]
+        X_train, X_test, y_train, y_test = train_test_split(X_new, y_new, test_size=0.33, random_state=0) # 67% training and 33% test // found out by 1 - test_size = 1 - 0.33 = 0.67 -> 67%
             
+        #scaling
+        sc = StandardScaler(with_mean=False)
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.transform(X_test)
+
+        
+        
+    #################### start select a model and show a plot###############################
+        with header: 
+            models = {'Linear': LinearRegression,
+                        'Lasso': Lasso,
+                        'Ridge': Ridge, 
+                        'ElasticNet': ElasticNet} 
+                    
+            st.subheader("Predicted the price of feriliser")
+            select_model = st.selectbox(
+                            'Select model:',
+                            ("Linear", "Lasso", "Ridge", "ElasticNet"))
+                    
+            st.write('You selected:', select_model)  
+        
+        
+        # function that takes in the select model, list of models, test and training data
+        def plotmodel(select_model, models, X_train, y_train, X_test, y_test):
+            for key, value in models.items():
+                    #st.write(i)
+                if select_model == key:
+                        
+                    # model
+                    model = value()
+                    model.fit(X_train, y_train)
+                    #Predict the response for test dataset
+                    y_pred = model.predict(X_test)
+                    with col1:
+                    # Dataframe of results
+                        model_result=pd.DataFrame({ 'Actual':y_test, 'Predicted':y_pred, 'Difference': (y_test - y_pred)})
+                        st.write(model_result.head(5))
+                    with col2:
+                        mae = round(mean_absolute_error(y_test,y_pred), 2)
+                        st.write("mean_absolute_error", mae)
+                        mse = round(mean_squared_error(y_test,y_pred), 2)
+                        st.write("mean_squared_error", mse)
+                        model_rmse = round(math.sqrt(mse), 2)
+                        st.write("Root mean squared error", model_rmse)
+                        r2 = round(r2_score(y_test,y_pred), 2)
+                        st.write("R2 score:", r2)
+                        precision = round(model.score(X_train, y_train), 2)
+                        st.write("Training model precision:", precision)
+                    
                 
-    
-                #plot actual vs predicted and ols trendline
-                fig_model = px.scatter(model_result, x='Actual', y='Predicted', opacity=0.65,  trendline="ols", title="Actual Vs Predicted using %s" % (key),  trendline_color_override='darkblue')
-                
-                #plot on streamlit
-                st.plotly_chart(fig_model, use_container_width=True)
                     
         
-    plotmodel(select_model, models, X_train, y_train, X_test, y_test)
-    
+                    #plot actual vs predicted and ols trendline
+                    fig_model = px.scatter(model_result, x='Actual', y='Predicted', opacity=0.65,  trendline="ols", title="Actual Vs Predicted using %s" % (key),  trendline_color_override='darkblue')
+                    
+                    #plot on streamlit
+                    st.plotly_chart(fig_model, use_container_width=True)
+                        
+            
+        plotmodel(select_model, models, X_train, y_train, X_test, y_test)
+        
 
-#################### End select a model and show a plot###############################
-    
-    
-    
-    
+    #################### End select a model and show a plot###############################
+        
+        
+        
+        
 
 
